63b60d082b80f44c6e3a5ac5ef268f47d7578bc5
Teddy_changik <aackddlr@gmail.com>
2020-12-30 22:50:10 +0900

    add NetworkModule.kt

--- app/src/main/java/com/clean/simplenote/view/SimpleNoteApplication.kt
+++ app/src/main/java/com/clean/simplenote/view/SimpleNoteApplication.kt
@@ -1,11 +1,21 @@
 package com.clean.simplenote.view
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.BuildConfig
+import com.orhanobut.logger.Logger
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SimpleNoteApplication : Application(){
     override fun onCreate() {
         super.onCreate()
 
+        Logger.addLogAdapter(object : AndroidLogAdapter() {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
         // initialize Hilt Application
     }
 
